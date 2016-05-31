6ac57c543f63cf8cbb1811df08bd0b0313bdb42d
Alan Denison and Bang Nguyen <alan@>
2016-05-31 11:24:35 -0700

    added second function for gallons

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ var inLiters = function (number1, converter) {
 var converter = 0.26417;
 var number1 = parseInt(prompt("Type in the gallon"));
 
-alert(inLiters(number1, converter));
+var inGallons = function (number1, converter) {
+  return number1 * converter;
+}
+
+var converter = 0.26417;
+var number1 = parseInt(prompt("Type in the liters"));
+
+
+alert(inGallons(number1, converter));
